5b96ad86bc9343255ac538dad17377da2474f215
Cynthia <48297339+azelie27@users.noreply.github.com>
2019-03-13 11:51:00 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,7 @@
 
 
 
-var fn = function (){confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please!')};
-window.onload = function () {setTimeout (function(){fn(); window.onload()},10000)};
-
-
-
-if (confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please!'));
-
-
-{location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";}
-
-
-/*setTimeout(confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please!')), 5000;*/
-
-
-/*if (confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please!')
-
-
-{
-location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-
-}
-
-*/
+*/setTimeout (alert('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please!')); 10000/*
 
 
 
